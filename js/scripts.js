db7daa80c4b677e380df96d26506315fa34fa5bb
Amanda Otero <mandystar018@gmail.com>
2020-08-12 09:42:15 -0700

    This commit will add Index.html and the body, style.css and id story and Script funcitions and const
    Co-authored-by: David Cole <davidtcole@yahoo.com>
    Co-Authored-By: Michael Watts <wattsjmichael@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+  const inputPerson = $("inputPerson").val
+  const inputPlace = $("inputPlace").val
+  const inputVerb = $("inputVerb").val
+  const inputExclamation = $("inputExclamation").val
+})
