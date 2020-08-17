5d5d2fd1f9710d8a9d86bedcd98e4dedd12072aa
Amanda Otero <mandystar018@gmail.com>
2020-08-17 12:04:59 -0700

    Add .forEach and loops
    Co-authored-by: Jillian Gibson  <jillian.l.gibson@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,90 @@
-$(document).ready(function(){
-  const inputPerson = $("inputPerson").val
-  const inputPlace = $("inputPlace").val
-  const inputVerb = $("inputVerb").val
-  const inputExclamation = $("inputExclamation").val
-})
+$(document).ready(function() {
+  $("#formOne").submit(function(event) {
+    event.preventDefault();
+    const person1Input = $("input#person1").val();
+    const person2Input = $("input#person2").val();
+    const animalInput= $("input#animal").val();
+    const exclamationInput = $("input#exclamation").val();
+    const verbInput = $("input#verb").val();
+    const nounInput = $("input#noun").val();
+
+    const spans = [".person1", ".person2", ".animal", ".exclamation", ".verb", ".noun"];
+    const inputs = [person1Input, person2Input, animalInput, exclamationInput,verbInput, nounInput];
+    let newInput = 0;
+
+    spans.forEach(function(element){
+      $(element).text(inputs[newInput]);
+      newInput += 1;
+   });
+   $("#story").show();
+  });
+});
+
+
+
+
+
+    // const userInput = ['input#person1', 'input#person2', 'input#animal', 'input#exclamation', 'input#verb', 'input#noun'];
+    // userInput.forEach(function (input){
+    // $('input#person1').show(input);
+    // $('input#person2').show(input);
+    // $('input#animal').show(input);
+    // $('input#exclamation').show(input);
+    // $('input#verb').show(input);
+    // $('input#noun').show(input);
+    
+    // });
+
+    
+//     person1Input = $("input#person1").val();
+//     const person2Input = $("input#person2").val();
+//     const animalInput= $("input#animal").val();
+//     const exclamationInput = $("input#exclamation").val();
+//     const verbInput = $("input#verb").val();
+//     const nounInput = $("input#noun").val();
+
+//     $("person1").text(person1Input);
+//     $(".person2").text(person2Input);
+//     $(".animal").text(animalInput);
+//     $(".exclamation").text(exclamationInput);
+//     $(".verb").text(verbInput);
+//     $(".noun").text(nounInput);
+
+//     $("#story").show();
+//   });
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ORIGINAL CODE
+// $(document).ready(function() {
+//   $("#formOne").submit(function(event) {
+//     event.preventDefault();
+//     const person1Input = $("input#person1").val();
+//     const person2Input = $("input#person2").val();
+//     const animalInput= $("input#animal").val();
+//     const exclamationInput = $("input#exclamation").val();
+//     const verbInput = $("input#verb").val();
+//     const nounInput = $("input#noun").val();
+
+//     $("person1").text(person1Input);
+//     $(".person2").text(person2Input);
+//     $(".animal").text(animalInput);
+//     $(".exclamation").text(exclamationInput);
+//     $(".verb").text(verbInput);
+//     $(".noun").text(nounInput);
+
+//     $("#story").show();
+//   });
+// });
